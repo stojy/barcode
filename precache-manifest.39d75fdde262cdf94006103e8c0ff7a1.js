0da70efb19b9c9bc102f26f9b5dd6f9722184b97
stoj <tibor@stoj.net>
2020-08-04 22:28:09 +0800

    Updates

---
+++ precache-manifest.39d75fdde262cdf94006103e8c0ff7a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6422597d8afd289e02ee4ba033ce2602",
+    "url": "/barcode/index.html"
+  },
+  {
+    "revision": "27731aad4453dd7d565a",
+    "url": "/barcode/static/css/main.3b8b63c6.chunk.css"
+  },
+  {
+    "revision": "362872fa0b6f2c7e3f80",
+    "url": "/barcode/static/js/2.cccc3934.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/barcode/static/js/2.cccc3934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27731aad4453dd7d565a",
+    "url": "/barcode/static/js/main.275e456f.chunk.js"
+  },
+  {
+    "revision": "581a13a03ffdff9fed79",
+    "url": "/barcode/static/js/runtime-main.482fbc80.js"
+  }
+]);
