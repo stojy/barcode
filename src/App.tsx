d14b51a2dcad540a77499e33a603163a1ddcb39a
stoj <tibor@stoj.net>
2020-08-04 21:38:34 +0800

    show/hide start/stop via a bit of state magic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {BarcodeQuagga} from './components/BarcodeQuagga'
 
@@ -7,8 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <div id='captureTarget' />
         <BarcodeQuagga/>
       </header>
