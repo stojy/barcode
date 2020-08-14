69be1bdbce5e84a91ff53aa0166e110a784d7719
stoj <tibor@stoj.net>
2020-08-14 16:35:20 +0800

    changed App to a function component

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,20 @@ import { Barcode } from './components/Barcode'
 
 import logo from './barcode.png'
 
-class App extends React.Component<{}, {}> {
+export function App() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" height="300px" />
+          <br />
+          <br />
 
-  constructor(props: any) {
-    super(props);
-
-    console.log(`app.tsx ctor: state=${JSON.stringify(this.state)}`)
-  }
-
-  render() {
-    return (
-      <>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" height="300px" />
-            <br />
-            <br />
-
-            <Barcode />
-          </header>
-        </div>
-      </>
-    )
-  }
+          <Barcode />
+        </header>
+      </div>
+    </>
+  )
 }
 
 export default App;
