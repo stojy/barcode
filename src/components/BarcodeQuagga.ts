49de5118749d55b3bfd3e2bd796f46e956b77f83
Tibor Stojanovski <tibor.stojanovski@rwwa.com.au>
2020-08-24 18:05:22 +0800

    added ts definition and removed warnings

--- src/components/BarcodeQuagga.ts
+++ src/components/BarcodeQuagga.ts
@@ -1,53 +1,52 @@
-import Quagga from 'quagga'
- 
-type OnDetectedCallback = (code: string) => void;
+/* istanbul ignore file */
+import Quagga, { QuaggaJSResultObject } from 'quagga'
+
+type OnDetectedCallback = (code: string) => void
 
 export interface IBarcode {
-  width: number,
-  height: number,
-  onDetected: OnDetectedCallback,
+  width: number
+  height: number
+  onDetected: OnDetectedCallback
 }
 
 export class BarcodeQuagga {
-  _onDetected!: OnDetectedCallback;
+  _onDetected!: OnDetectedCallback
 
-  start({width, height, onDetected}: IBarcode) {
+  start({ width, height, onDetected }: IBarcode): void {
     this._onDetected = onDetected
-
-    const self = this
 
     const config = {
       numOfWorkers: navigator.hardwareConcurrency,
       locate: true,
       inputStream: {
-        name: "Live",
-        type: "LiveStream",
+        name: 'Live',
+        type: 'LiveStream',
         target: '#captureTarget',
         constraints: {
           width,
           height,
-          facingMode: "environment"
+          facingMode: 'environment',
         },
         area: {
-          top: "0%",
-          right: "0%",
-          left: "0%",
-          bottom: "0%"
+          top: '0%',
+          right: '0%',
+          left: '0%',
+          bottom: '0%',
         },
       },
       decoder: {
-        readers: ["code_128_reader"],
+        readers: ['code_128_reader'],
         debug: {
           drawBoundingBox: false,
           showFrequency: false,
           drawScanline: false,
-          showPattern: false
+          showPattern: false,
         },
-        multiple: false
+        multiple: false,
       },
       locator: {
         halfSample: true,
-        patchSize: "large", // x-small, small, medium, large, x-large
+        patchSize: 'large', // x-small, small, medium, large, x-large
         debug: {
           showCanvas: false,
           showPatches: false,
@@ -59,56 +58,55 @@ export class BarcodeQuagga {
           boxFromPatches: {
             showTransformed: false,
             showTransformedBox: false,
-            showBB: false
-          }
-        }
-      }
-    };
-    console.log(`config: ${JSON.stringify(config)}`)
+            showBB: false,
+          },
+        },
+      },
+    }
 
-    Quagga.init(config, function (err: any) {
-      if (err) {
-        console.log(err);
-        return
-      }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    Quagga.init(config, (err: any) => {
+      if (err) throw Error(err)
 
-      console.log("initialization finished");
-      Quagga.onDetected(self.handleDetected.bind(self));
-      Quagga.start();
-    });
+      const drawingCanvas = Quagga.canvas.dom.overlay
+      drawingCanvas.style.display = 'none'
+
+      Quagga.onDetected(this.handleDetected.bind(this))
+      Quagga.start()
+    })
   }
 
-  stop() {
-    Quagga.offDetected(this.handleDetected);
+  stop(): void {
+    Quagga.offDetected(this.handleDetected)
     Quagga.stop()
   }
 
-  handleDetected(data: any) {
-    this._onDetected(data.codeResult.code);
+  handleDetected(data: QuaggaJSResultObject): void {
+    this._onDetected(data.codeResult.code)
   }
 
-  getCapabilities() : MediaTrackCapabilities | null {
-    let capabilities : MediaTrackCapabilities | null = null;
+  getCapabilities(): MediaTrackCapabilities | null {
+    let capabilities: MediaTrackCapabilities | null = null
 
-    var track = Quagga.CameraAccess.getActiveTrack() as MediaStreamTrack;
+    var track = Quagga.CameraAccess.getActiveTrack() as MediaStreamTrack
     if (typeof track.getCapabilities === 'function') {
-        capabilities = track.getCapabilities();
+      capabilities = track.getCapabilities()
     }
 
     return capabilities
   }
 
-  async setTorch(enable: boolean) {
-    await Quagga.CameraAccess.getActiveTrack().applyConstraints({
-      advanced: [{ torch: enable } as any]
+  async setTorch(enable: boolean): Promise<void> {
+    return Quagga.CameraAccess.getActiveTrack().applyConstraints({
+      advanced: [{ torch: enable } as MediaTrackConstraintSet],
     })
-  }  
+  }
 
   getCameraResolution(): [number, number] {
     // use low(er) resolution (e.g. 480px) instead of the camera's maximum allowed resolution to reduce processing overheads.. also not neeed to read a barcode
     let width = 480
     let height = 240
-  
+
     // camera screen/width is taken from a landscape perspective, so..
     // - for a real device running mobi (i.e. in portrait) the values are reversed
     // - for a simulated device running mobi (i.e. desktop development of mobi) OR desktop the values are left unchanged
@@ -118,4 +116,3 @@ export class BarcodeQuagga {
     return [width, height]
   }
 }
-
